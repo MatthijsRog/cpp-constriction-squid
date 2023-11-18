abc97606a8b0baa386ee95103d6e58e57cf0d570
Matthijs Rog <rogm@vuw.leidenuniv.nl>
2023-11-18 21:18:37 +0100

    Docs added.

---
+++ docs/html/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['main_0',['main',['../_main_8cpp.html#ac0f2228420376f4db7e1274f2b41667c',1,'Main.cpp']]],
+  ['main_2ecpp_1',['Main.cpp',['../_main_8cpp.html',1,'']]]
+];
--- src/Main.cpp
+++ src/Main.cpp
@@ -4,6 +4,13 @@
 
 #include <iostream>
 
+
+/**
+ * @brief A brief description of main.
+ * @param param1 Description of the first parameter.
+ * @param param2 Description of the second parameter.
+ * @return Description of the return value.
+ */
 int main(int argc, const char * argv[]) {
     std::cout << "Hello, World!\n";
     return 0;
